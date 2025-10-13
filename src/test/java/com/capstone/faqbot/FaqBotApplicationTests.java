726ebe91b02a1f31efa6308dc4ffee92fcbfe1fe
41613 <mahaveer@tataelxsi.co.in>
2025-10-13 12:24:18 +0530

    test ci/c31

--- src/test/java/com/capstone/faqbot/FaqBotApplicationTests.java
+++ src/test/java/com/capstone/faqbot/FaqBotApplicationTests.java
@@ -3,11 +3,11 @@ package com.capstone.faqbot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class FaqBotApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+@SpringBootTest
+class FaqBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
